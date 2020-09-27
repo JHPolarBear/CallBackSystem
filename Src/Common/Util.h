3192fb70692ce5dcefabfbd3eff904f43b1b1e2c
JHPolarBear <jhpark8904@gmail.com>
2020-09-28 00:20:39 +0900

    Add Common and util header

---
+++ Src/Common/Util.h
@@ -0,0 +1,8 @@
+/***********************************************************
+- Header class for common use function
+- Written by: JHP
+***********************************************************/
+#pragma once
+
+// Return macro to Check null ptr
+#define RETURN(_val) {if(!(_val) == 0) return;}
