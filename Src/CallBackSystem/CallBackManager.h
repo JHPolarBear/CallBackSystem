6f256c1d5342a4a18fbeb51b4f2ba7678b309659
JHPolarBear <jhpark8904@gmail.com>
2021-02-17 19:02:54 +0900

    Restor callback manager

---
+++ Src/CallBackSystem/CallBackManager.h
@@ -0,0 +1,17 @@
+/***********************************************************
+- Main class managing observer class
+- Written by: JHP
+***********************************************************/
+#pragma once
+
+#include "../Subject/Subject.h"
+
+
+class CallbackManager : public Subject
+{
+public:
+	CallbackManager();
+	virtual ~CallbackManager();
+
+protected:
+}; 
